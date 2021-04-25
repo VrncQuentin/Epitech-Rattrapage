03fb4c21a62eff9ad65d3da8da19a5b554d9e5a1
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2021-04-25 15:11:20 +0200

    feat: added wrapper for the front to call the new back routes

--- Dashboard/src/API/back.js
+++ Dashboard/src/API/back.js
@@ -1,33 +1,48 @@
 import requester from "./requester";
 
-const baseUrl = "http://0.0.0.0:3000/user/"
+const baseUrl = "http://0.0.0.0:3000/"
+const userUrl = baseUrl + 'user/'
+const widgetUrl = baseUrl + 'widget/'
 
 export const newUser = (user, accessToken) => {
-    return new Promise(((resolve, reject) => {
-        requester.post(baseUrl + user + '/' + accessToken)
+    return new Promise((resolve, reject) => {
+        requester.post(userUrl + 'create/' + user + '/' + accessToken)
             .then(res => {
                 resolve(res)
             })
             .catch(e => reject(e))
-    }))
+    })
+}
+
+export const newWeatherWidget = (userId, widget) => {
+    return new Promise((resolve, reject) => {
+        requester.post(widgetUrl + 'weather/' + userId, {
+            ...widget,
+            user: {
+                connect: {id: userId}
+            }
+        })
+            .then(res => resolve(res))
+            .catch(e => reject(e))
+    })
 }
 
 export const getUser = (user) => {
-    return new Promise(((resolve, reject) => {
-        requester.get(baseUrl + user)
+    return new Promise((resolve, reject) => {
+        requester.get(userUrl + user)
             .then(res => {
                 resolve(res)
             })
             .catch(e => reject(e))
-    }))
+    })
 }
 
 export const updateUser = (user, value) => {
-    return new Promise(((resolve, reject) => {
-        requester.post(baseUrl +'update/' + user, value)
+    return new Promise((resolve, reject) => {
+        requester.post(userUrl +'update/' + user, value)
             .then(res => {
                 resolve(res)
             })
             .catch(e => reject(e))
-    }))
+    })
 }
