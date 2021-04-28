ee9929ad06c4f8610c6718c261ace7eeb28cb890
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2021-04-29 00:23:34 +0200

    feat: brought back api calls to spacex but with parameters this time

---
+++ Dashboard/src/SpaceX/api.js
@@ -0,0 +1,20 @@
+import requester from "./requester";
+
+//doc: https://docs.spacexdata.com/#07a29989-38e3-47fb-9f64-c132b5842ff0
+const baseUrl = "https://api.spacexdata.com/v3/"
+
+export const getRocketInfo = (rocket) => {
+    return new Promise((resolve, reject) => {
+        requester.get(baseUrl + 'rockets/' + rocket)
+            .then(r => resolve(r))
+            .catch(e => reject(e))
+    })
+}
+
+export const getInfo = (about) => {
+    return new Promise((resolve, reject) => {
+        requester.get(baseUrl + 'info/')
+            .then(r => resolve(r[about]))
+            .catch(e => reject(e))
+    })
+}
